679797213641b2107835f51035d62df4c9eb69c1
kingkcee <111048996+kingkcee@users.noreply.github.com>
2025-05-18 16:16:58 +0100

    fix: remove missing globals.css import

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 // pages/_app.js
 import ErrorBoundary from '../components/ErrorBoundary';
-import '../styles/globals.css'; // if you have global styles
+
 
 export default function MyApp({ Component, pageProps }) {
   return (
